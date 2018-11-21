42c8e0d8d62a00c14735e21e81fa0d49d3f4d1dc
lorena377 <alamo_roman@hotmail.com>
2018-11-21 12:12:07 -0500

    añade estructura basica en html y funcionalidad a los botones en js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const cifrar = document.getElementById("cifrar");
+function CIFRAR(){
+
+}
+cifrar.addEventListener('click',CIFRAR);
+
+const decifrar = document.getElementById("decifrar");
+
+function decifrar (){
+
+}
+decifrar.addEventListener('click', decifrar);
